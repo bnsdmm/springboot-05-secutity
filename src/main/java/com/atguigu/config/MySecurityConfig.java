e6ad9a927fa58324f3895e5619a471d8951b3e18
wangzhen <506826727@qq.com>
2019-11-12 17:14:39 +0800

    v1.0

---
+++ src/main/java/com/atguigu/config/MySecurityConfig.java
@@ -0,0 +1,37 @@
+package com.atguigu.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        //定制认证规则
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder()).withUser("zhangsan").password(new BCryptPasswordEncoder().encode("0000")).roles("VIP1", "VIP2")
+                .and()
+                .passwordEncoder(new BCryptPasswordEncoder()).withUser("lisi").password(new BCryptPasswordEncoder().encode("0000")).roles("VIP2", "VIP3")
+                .and()
+                .passwordEncoder(new BCryptPasswordEncoder()).withUser("wangwu").password(new BCryptPasswordEncoder().encode("0000")).roles("VIP1", "VIP3");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(auth);
+        //定制请求的授权规则
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
+        //开启自动配置登录功能
+        http.formLogin();
+        http.logout();
+
+    }
+}
---
+++ src/test/java/com/atguigu/secutity/Springboot05SecutityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.secutity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot05SecutityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
